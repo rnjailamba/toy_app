cf93f72a4611306c7650ca3c35967c863237af4c
rnjai <rnjailamba12@gmail.com>
2015-01-21 10:45:12 +0530

    Added password digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120042107) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20150120181026) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -26,5 +19,7 @@ ActiveRecord::Schema.define(version: 20150120042107) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
